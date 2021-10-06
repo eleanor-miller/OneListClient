fc2435400be6a02bcb4f65df6037817f38faeecc
Eleanor Miller <eleanormiller.dev@gmail.com>
2021-10-06 17:34:31 -0400

    Use JsonSerializer to *DE*serialize the stream inot a nice List of Item objects!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseBodyAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=eleanor-miller");
+
+            //                                              Describe the Shape of the data (array in JSON => List, Object in JSON => Item)
+            //                                                  v   v
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsStream);
+
+            // Back in the world of List/LINQ/C#
+            foreach (var item in items)
+            {
+                Console.WriteLine($"The task {item.text} was created on {item.created_at} and has a completion of {item.complete} ");
+            }
+        }
     }
-  }
 }
