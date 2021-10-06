e69f76eb502a492aac01d3280f21ad3fcb544266
Eleanor Miller <eleanormiller.dev@gmail.com>
2021-10-06 17:50:58 -0400

    Custom DateTime and Custom CompletedStatus

--- Item.cs
+++ Item.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace OneListClient
 {
     public class Item
@@ -5,7 +7,28 @@ namespace OneListClient
         public int id { get; set; }
         public string text { get; set; }
         public bool complete { get; set; }
-        public string created_at { get; set; }
-        public string updated_at { get; set; }
+        // Converted string to DateTime
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+
+        // Custom 'get' for a property
+        public string CompletedStatus
+        {
+            get
+            {
+                // if (complete)
+                // {
+                //     return "Completed";
+                // }
+                // else
+                // {
+                //     return "Not Completed";
+                // }
+
+                // Shorthand for the above =>  return   boolean expression  ?    value when true    :   value when false
+                return complete ? "Completed" : "Not Completed";
+            }
+        }
     }
 }
